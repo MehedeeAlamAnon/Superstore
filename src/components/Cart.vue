c36e982c21f6ca7b2fe6978b41b67691c97ef644
MEHEDEE ALAM <mehedeealam@gmail.com>
2020-12-05 16:49:16 +0600

    first commit

---
+++ src/components/Cart.vue
@@ -0,0 +1,44 @@
+<template>
+    <ul class="list-group">
+        
+        <li class="list-group-item list-group-item d-flex justify-content-between">
+            <span>Item</span>
+            <span>Price</span>
+        </li>
+
+        <li v-for="(item,index) in items" :key="index" class="list-group-item d-flex justify-content-between">
+            <span>{{item.title}}</span>
+            <button class="btn btn-sm btn-danger" @click="removeItem(index)">-</button>
+            <span>{{item.price}}</span>
+        </li>
+
+        <li class="list-group-item list-group-item d-flex justify-content-between">
+            <span>Total</span>
+            <span>{{totalPrice}}</span>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props:['items'],
+    computed:{
+        totalPrice(){
+            var total=0
+            this.items.forEach(item => {
+                total=total+parseFloat(item.price)
+            })
+            return total
+        }
+    },
+    methods:{
+        removeItem(index){
+            this.$emit('itemRemoved',index)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Inventory.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <div class="row">
+        <div class="col-sm-4" v-for="(item,index) in items" :key="index">
+            <div class="card">
+                <img :src="item.photo" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">{{item.title}}</h5>
+                <p class="card-text">{{item.price}}</p>
+                <a @click="addToCart(item)" class="btn btn-primary">+ADD</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    props:['items'],
+    methods:{
+        addToCart(item){
+            this.$emit('newItemAdded',item)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
